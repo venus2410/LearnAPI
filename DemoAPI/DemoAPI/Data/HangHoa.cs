5b26c6649f764d288b64e6a12e0fb718befffd7d
venus2410 <nguyencongdanh2410@gmail.com>
2023-04-26 23:16:34 +0700

    ef table

---
+++ DemoAPI/DemoAPI/Data/HangHoa.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoAPI.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string TenHang { get; set; }
+        [Range(0, double.MaxValue)]
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public LoaiHang LoaiHang { get; set; }
+    }
+}
---
+++ DemoAPI/DemoAPI/Data/MyDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoAPI.Data
+{
+    public class MyDBContext:DbContext      
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=DemoAPI;Integrated Security=True;TrustServerCertificate=true;");
+        }
+        public DbSet<HangHoa> HangHoas { get; set;}
+        public DbSet<LoaiHang> LoaiHangs { get; set;}
+    }
+}
